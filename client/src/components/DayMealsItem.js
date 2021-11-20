bd48bc5a84360a2ac23e0a5c745abe871fe0b6c9
Spencer Malott <spencermalott@Spencers-MBP.hitronhub.home>
2021-11-19 16:21:37 -0800

    add storybook, add mui, start working on DayMealsItem and DayMeals components

---
+++ client/src/components/DayMealsItem.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+import ButtonBase from '@mui/material/ButtonBase';
+
+import '../index.css';
+
+export default function DayMealsItem(props) {
+  return (
+    
+    <Paper sx={{ p: 2, margin: 'auto', maxWidth: 500, flexGrow: 1 }}>
+      <Grid container spacing={2}>
+        <Grid item>
+          <ButtonBase sx={{ width: 128, height: 128 }}>
+          <img src="/https://mui.com/static/images/grid/complex.jpg" />
+          </ButtonBase>
+        </Grid>
+        <Grid item xs={12} sm container>
+          
+            <Grid item xs>
+              <Typography gutterBottom variant="subtitle1" component="div">
+                Meal Prop
+              </Typography>
+              <Typography variant="body2" gutterBottom>
+                Recipe Title Prop
+              </Typography>
+              <ButtonBase sx={{ cursor: 'pointer' }} variant="body2" color="text.secondary">
+                Recipe Details
+              </ButtonBase>
+            </Grid>
+          
+        </Grid>
+      </Grid>
+    </Paper>
+    
+  );
+
+};
--- client/src/index.js
+++ client/src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    <Path2D></Path2D>
+    
   </React.StrictMode>,
   document.getElementById('root')
 
