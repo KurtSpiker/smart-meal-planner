79788d7aba862a3440dc26f109ff0afab2683eeb
Spencer Malott <spencermalott@Spencers-MBP.hitronhub.home>
2021-11-26 11:15:01 -0800

    update counter format to only allow numbers. Use react-number-format dependency

--- client/src/components/Counter.js
+++ client/src/components/Counter.js
@@ -2,17 +2,20 @@ import React from "react";
 import useCounter from '../hooks/useCounter';
 import ButtonGroup from "@mui/material/ButtonGroup";
 import Button from '@mui/material/Button';
+import NumberFormat from 'react-number-format'
 
 const Counter = function (props) {
   const { quantity, listName, ingredientId } = props
-  const { count, handleDecrement, handleIncrement } = useCounter(quantity, listName, ingredientId);
+  const { count, setCount, handleDecrement, handleIncrement } = useCounter(quantity, listName, ingredientId);
 
   return (
     <ButtonGroup aria-label="small outlined button group" >
       <Button onClick={() => handleIncrement()}>+</Button>
-      <Button>
-        {count}
-      </Button>
+        <NumberFormat 
+          value={count}
+          onChange={(event) => setCount(event.target.value)}
+          width="70px"
+        />
       <Button onClick={() => handleDecrement()}>-</Button>
     </ButtonGroup>
   );
