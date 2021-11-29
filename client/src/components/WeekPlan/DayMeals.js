072af4205c0537d2728adfe3cbfa7ce16d820d2a
Spencer Malott <spencermalott@Spencers-MBP.hitronhub.home>
2021-11-28 17:34:04 -0800

    tinkering with styles

--- client/src/components/WeekPlan/DayMeals.js
+++ client/src/components/WeekPlan/DayMeals.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import { Grid, Typography, Paper } from '@mui/material';
 import DayMealsItem from './DayMealsItem'
+import { styled } from '@mui/material/styles';
 
 export default function DayMeals(props) {
   const mealTypes = ["breakfast", "lunch", "dinner"];
   const { meals, dayOfWeek, removeMeal } = props;
 
+  const Title = styled(Typography)
+  (`
+    
+      font-family: 'Questrial', sans-serif;
+      text-transform: capitalize
+
+  `);
+
   return (
     <Grid sx={{ paddingBottom: "20px" }}>
       <Grid>
-        <Typography align="center" variant="h4" component="div">
+        <Title align="center" variant="h4" component="div">
           {dayOfWeek}
-        </Typography>
+        </Title>
       </Grid>
       <Grid marginTop={0} container spacing={2} columns={3} wrap={"nowrap"}>
         {
